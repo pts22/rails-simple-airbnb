e9d9b4f6cfdc995fead19ccc5b32f4a65d632e44
Tommy Siu <tommysiu0@hotmail.com>
2018-11-14 22:10:07 +0800

    v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Tree House',
+    address: '187 Fuming Road',
+    description: 'beautiful tree house with outdoor patio',
+    price_per_night: 30,
+    number_of_guests: 2
+  },
+  {
+    name: 'The Mansion',
+    address: 'Hongqiao airport',
+    description: 'good for parties and drinks included',
+    price_per_night: 100,
+    number_of_guests: 5
+  },
+  {
+    name: 'Wework',
+    address: 'West Nanjing Road',
+    description: 'We have 2 beds on the 4th floor with unlimited beer',
+    price_per_night: 20,
+    number_of_guests: 2
+  },
+]
+Flat.create!(flats_attributes)
+# give error message
+puts 'Finished!'
+
+
